682261afc53db3cb0ef0a3f6576dee13530815d1
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-09-15 17:31:07 +0600

    make first controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\DemoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::put('/hello', function () {
-    return "Hellow Laravel";
-});
+Route::get('/hello', [DemoController::class, 'DemoAction']);
