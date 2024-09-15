d8a91a417899dbeb09d182a1541675a520090efb
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-09-15 19:13:10 +0600

    JSON Controller-1

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Httpgi\Request;
+use Illuminate\Http\Request;
 
 class JsonController extends Controller {
-    function JsonAction(Request $request) {
-        $name = $request->name;
-        $age = $request->age;
-        return "Name is ${name}, age is = ${age}";
+    function JsonAction(Request $request): array {
+
+        return $request->input();
     }
 }
