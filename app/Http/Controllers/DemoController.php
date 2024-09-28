bd232681d8db2da92de70b1261cbe9c4d0cb5095
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-09-29 00:23:11 +0600

    Log

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class DemoController extends Controller {
-    function DemoAction(Request $request): array {
-
-        return $request->getAcceptableContentTypes();
+    function DemoAction(Request $request): int {
+        $num1 = $request->num1;
+        $num2 = $request->num2;
+        $sum = $num1 + $num2;
+        Log::info("$num1 + $num2 = " . $sum);
+        return $sum;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\DemoController;
 use App\Http\Controllers\FormController;
+use App\Http\Controllers\GreetingsController;
 use App\Http\Controllers\PracticeParameterController;
 use App\Http\Controllers\PramController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // ফর্ম দেখানোর Route (GET)
@@ -38,3 +40,38 @@ Route::get('/hello', [DemoController::class, 'DemoAction']);
 Route::post("/demo1", [ProductController::class, 'Demo1']);
 Route::get("/demo2", [ProductController::class, 'Demo2']);
 Route::get("/demo3", [ProductController::class, 'Demo3']);
+
+Route::get('/test', [GreetingsController::class, 'sayHi']);
+
+Route::post('/form-submit', function (Request $request) {
+
+    $email = $request->input('email');
+
+    if ($email) {
+
+        return response()->json([
+            "status" => "success",
+            "message" => "Form submitted successfully.",
+            "email" => $email,
+        ]);
+
+    } else {
+
+        return response()->json([
+            "status" => "failed",
+            "message" => "Form submission failed.",
+        ]);
+
+    }
+
+});
+
+Route::get('/client-ip', function (Request $request) {
+
+    $clientIp = $request->ip();
+
+    return $clientIp;
+
+});
+
+Route::get('/DemoAction/{num1}/{num2}', [DemoController::class, 'DemoAction']);
