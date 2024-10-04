bc6acc02fa4a9552a03a823302ef0d95c8ad69c1
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-10-05 02:19:31 +0600

    for loop file complite

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,11 @@
 
-@switch($result)
-@case(100)
-    <h1>Result is hundred</h1>
-    @break
-@case(1000)
-    <h1>Result is thousend</h1>
-    @break
-@case(100000)
-    <h1>Result is lakh</h1>
-    @break
-@default
-    <h1>Result is not in our range</h1>
+<ul>
+@for($i=0; $i<100; $i=$i+1)
 
-@endswitch
+        <li>{{ $i }}</li>
 
 
 
+
+@endfor()
+</ul>
