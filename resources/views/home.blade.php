6467b1728ce95f024b81018417efa30cc16ad520
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-10-05 19:11:18 +0600

    complite

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,10 @@
 @extends('Layout.app')
 @section('content')
-
+@include('component.hero')
+@include('component.howWeDo')
+@include('component.pricing')
+@include('component.team')
+@include('component.newsletter')
 @endsection
 
 
