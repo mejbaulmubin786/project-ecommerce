0f3d1f383bc6d7ec690fa125ebf8b56bdcd0a977
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-10-14 05:42:01 +0600

    Profiles  is complite

--- database/migrations/2024_10_13_215919_create_profiles.php
+++ database/migrations/2024_10_13_215919_create_profiles.php
@@ -4,24 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('firstName', 50);
+            $table->string('lastName', 50);
+            $table->string('mobile', 50);
+            $table->string('city', 50);
+            $table->string('shippingAddress', 1000);
+            $table->string('email', 50)->unique();
+            $table->foreign('email')->references('email')->on('users')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('profiles');
     }
 };
