65ed9e7fbfb9dc8e223edad82e1bda38284d18aa
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-10-29 08:02:26 +0600

    72 complite

--- Module-18/app/Http/Controllers/DemoController.php
+++ Module-18/app/Http/Controllers/DemoController.php
@@ -9,26 +9,9 @@ use Illuminate\Support\Facades\DB;
 
 class DemoController extends Controller {
     public function DemoAction() { // Use 'public' visibility and follow proper naming conventions
-        $count = DB::table('products')->count();
+        $products = DB::table('products')->select('title', 'price')->get();
 
-        // টেবিলের price কলামের সর্বোচ্চ মান
-        $maxPrice = DB::table('products')->max('price');
 
-        // টেবিলের price কলামের সর্বনিম্ন মান
-        $minPrice = DB::table('products')->min('price');
-
-        // টেবিলের price কলামের গড় মান
-        $averagePrice = DB::table('products')->avg('price');
-
-        // টেবিলের price কলামের সমস্ত মানের যোগফল
-        $totalPrice = DB::table('products')->sum('price');
-
-        return [
-            'Total Rows' => $count,
-            'Max Price' => $maxPrice,
-            'Min Price' => $minPrice,
-            'Average Price' => $averagePrice,
-            'Total Price Sum' => $totalPrice,
-        ];
+        return $products;
     }
 }
