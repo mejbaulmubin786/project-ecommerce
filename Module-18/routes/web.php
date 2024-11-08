58c353fd5bd27540cc1a542c400f32453ca448a8
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-11-08 23:20:48 +0600

    89

--- Module-18/routes/web.php
+++ Module-18/routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\DemoController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/', [DemoController::class, 'DemoAction']);
+Route::get('/demo', [DemoController::class, 'DemoAction']);
