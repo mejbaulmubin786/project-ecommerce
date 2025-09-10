3b0953e6df06c1722121eab672ee375c17a28d27
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-09-10 13:05:47 +0600

    cit-project ecommerce start

--- Old/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PracticeController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [HomeController::class, 'page']);
-
-Route::get('/country/{name}/{divition}', [PracticeController::class, 'practice']);
