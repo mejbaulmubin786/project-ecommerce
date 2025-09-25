f9d7e5bc4b50e731d0ca362e0f806c6b19bb9693
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-09-25 23:04:36 +0600

    react router dom install and some pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} > </Route>
+      <Route path="/shop" element={<Shop />} > </Route >
+    </>
+  )
+);
+
 
 const App = () => {
   return (
-    <div>
-      <h1>Mej</h1>
-      <h2>Mej</h2>
-    </div>
+
+    <RouterProvider router={router} />
+
   )
 }
 
